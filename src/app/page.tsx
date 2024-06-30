6572e5406fc2e037e67915003a1cbb6c160878f3
yusuf-ishaku <kingshakzy@gmail.com>
2024-06-30 03:17:28 +0100

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24">
+    <main className="flex flex-col pt-10 items-center w-[100vw] h-[100vh] overflow-hidden">
+
       <Image
-      src="/appliftlogo.png"
-      alt="Applift"
-      height={100}
-      width={100}
-      className="dark:invert"
+        src="/appliftlogo.png"
+        alt="Applift"
+        height={100}
+        width={100}
+        className="dark:invert"
       ></Image>
       <Image
-      src="/software-illustration.png"
-      alt="Illustrate"
-      height={50}
-      width={100}
+        src="/software-illustration.png"
+        alt="Illustrate"
+        height={100}
+        width={200}
       ></Image>
-    </main> 
+        <div className="text-white my-10">
+        We are under construction
+      </div>
+      <div className="w-[100vw] flex flex-row justify-center -z-50">
+      <div className="box1 rotate-45"></div>
+      </div>
+    
+    </main>
   );
 }
