436941fb3c53f74229ec8d89f524b7bea5399cab
yusuf-ishaku <kingshakzy@gmail.com>
2024-07-14 01:00:35 +0100

    Page ready for take off

---
+++ src/app/components/WIP.tsx
@@ -0,0 +1,9 @@
+export const WIP = () => {
+    return (
+        <span
+        className="w-full text-black text-xl mx-2"
+        >
+          Work in progress
+        </span>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
+import { WIP } from "./components/WIP";
 
 export default function Home() {
   return (
     <main className="flex flex-col pt-10 items-center w-[100vw] h-[100vh] overflow-hidden">
-
       <Image
         src="/appliftlogo.png"
         alt="Applift"
         height={100}
         width={100}
-        className="dark:invert"
+        className="dark:invert mb-5"
       ></Image>
       <Image
         src="/software-illustration.png"
@@ -17,13 +17,48 @@ export default function Home() {
         height={100}
         width={200}
       ></Image>
-        <div className="text-white my-10">
-        We are under construction
+      <section className="w-full text-white px-5 text-center relative">
+        <h1 className="text-2xl mt-5">
+          We're Under <br></br>Construction
+        </h1>
+        <p className="text-md">
+          Our new site is on the way!<br></br> In the meantime, you can reach us
+          at <span className="text-[#86C7FE]">hello@applift.com</span>.{" "}
+          <br></br> Thanks for your patience.
+        </p>
+        <div className="dcr_line">
+          <div className="w-full overflow-hidden flex flex-row items-center">
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+
+          </div>
+        </div>
+        <div className="dcr_line2">
+        <div className="w-full overflow-hidden flex flex-row items-center ">
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+            <WIP></WIP>
+
+          </div>
+        </div>
+      </section>
+      <div className="sm:hidden w-[100vw] relative -top-[4.5rem] flex flex-col justify-center items-center -z-50">
+        <div className="box1"></div>
+        <div className="box2"></div>
       </div>
-      <div className="w-[100vw] flex flex-row justify-center items-center -z-50">
-      <div className="box1 rotate-45"></div>
+      <div className=" w-auto relative  flex flex-row justify-center items-center -z-50">
+        <div className="box3 "></div>
+        <div className="box4"></div>
       </div>
-    
     </main>
   );
 }
